912904649f0eb03e58f863f39a393bffac3da2e7
Alberto Ruis <alber.ruis2886@gmail.com>
2023-10-17 23:31:07 -0300

    se dio funcionalidad a prestamos.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,11 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
-
       colors: {
         "primary-libra-violet": "#A783F8",
         "secondary-libra-violet": "#8B5CF6",
-      }
-      
+      },
     },
   },
   plugins: [],
-}
+};
